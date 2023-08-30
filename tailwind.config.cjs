c0e89d3d7a74721b42d49f5989ac616d5385106d
kitkovsky <okitkowski114@gmail.com>
2023-08-30 19:26:15 +0200

    add main page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        white: '#d1d0c5',
+        gray40: '#646669',
+        gray60: '#323437',
+        gray80: '#2c2e31',
+        black: '#232427',
+        orange: '#e2b514',
+        'dark-orange': '#bc9710',
+      },
+    },
+  },
+  plugins: [],
 }
